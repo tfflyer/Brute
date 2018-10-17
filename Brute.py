2b012f8f7323fc48a1583e0175c0138280461e61
王腾飞 <39651321+tfflyer@users.noreply.github.com>
2018-10-17 19:17:59 +0800

    new
    test

---
+++ Brute.py
@@ -0,0 +1,44 @@
+import re
+import time
+from itertools import permutations
+
+
+def solve(puzzle):
+    puzzle = puzzle.upper()                            # 统一转换为大写
+    words = re.findall('[A-Z]+', puzzle)               # 正则表达式提取出字母
+    unique_characters = set(''.join(words))
+    assert len(unique_characters) <= 10, 'Too many letters'  # 只有0—9十个数
+    first_letters = {word[0] for word in words}    # 首字母
+    n = len(first_letters)
+    sorted_characters = ''.join(first_letters) + \
+                        ''.join(unique_characters - first_letters)
+    characters = tuple(ord(c) for c in sorted_characters)
+    digits = tuple(ord(str(c)) for c in range(10))
+    zero = digits[0]     # 数字0不能是第一位
+    print('蛮力测试：\n')
+    for guess in permutations(digits, len(characters)):
+        if zero not in guess[:n]:
+            equation = puzzle.translate(dict(zip(characters, guess)))     # 转换字符
+            dig = tuple(chr(c) for c in guess)
+            if eval(equation):               # 将字符串string对象转化为有效的表达式参与求值运算返回计算结果
+                print(sorted_characters)
+                print(dig)
+                return equation
+
+
+if __name__ == '__main__':
+    start = time.clock()
+    # puzzle = "send + more == money"
+    puzzle = input("请输入字母算术式,并用‘==’连接：")
+    try:
+        print(puzzle.upper())
+        solution = solve(puzzle)
+        end = time.clock()
+        if solution:
+            print(puzzle)
+            print('符合条件的结果：\n'+solution)
+            print("final is in ", round(end - start), 's')
+        else:
+            print("Nothing match this")
+    except Exception as e:
+        print("Error: " + str(e))
